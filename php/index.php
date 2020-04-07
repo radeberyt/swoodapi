2b480c2f59174b91f09015e6a11e502d53e7a621
radeber <44522797+radeberyt@users.noreply.github.com>
2020-04-07 19:41:07 +0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -2,5 +2,28 @@
 ////////////////////////////////////////
 // Version 1 of StoneWood Project API //
 ////////////////////////////////////////
+require __DIR__ . '/MinecraftPing.php';
+require __DIR__ . '/MinecraftPingException.php';
+
+///////////// CONFIG /////////////
+$SERVER_URL = null; // Don't set it if you dont have a server url
+$SERVER_IP 	= 'play.lastcraft.com';
+$PORT 			= '25565';
+try
+{
+		$Query = new MinecraftPing( $SERVER_IP, $PORT );
+
+		$data = $Query->Query();
+		$status = 'online';
+}
+catch( MinecraftPingException $e )
+{
+		echo $e->getMessage();
+		$status = 'offline';
+}
+finally
+{
+		$Query->Close();
+}
 
 ?>
